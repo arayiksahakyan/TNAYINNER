a6f4ed579541f779a89ed83da0f460705f88e50b
arayiksahakyan <149326909+arayiksahakyan@users.noreply.github.com>
2023-10-29 23:28:38 +0400

    Update tn 1.cpp

--- tn 1.cpp
+++ tn 1.cpp
@@ -3,18 +3,31 @@ using namespace std;
 
 void findMinMax(int x, int y, int z) {
     int minval=min(min(x, y), z);
+    
     int maxval=max(max(x, y), z);
 
+
+    
     cout<<"min-"<<minval<<", max-"<<maxval<<endl;
 }
+
+
+
+
 int main(int argc, char* argv[]) {
     if(argc != 4) {
         cout<<"Usage: ./program_name x y z"<<endl;
         return 1;
     }
+
+
+    
     int x=atoi(argv[1]);
+    
     int y=atoi(argv[2]);
+    
     int z=atoi(argv[3]);
+    
     findMinMax(x, y, z);
     return 0;
 }
